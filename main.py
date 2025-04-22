ab5f13e420c9b48a7c4096ab0561c87cdd3da9b7
Guilherme Figueroa Jacques <163030792+guilherme-jacques@users.noreply.github.com>
2025-04-22 11:28:07 -0300

    atualização que tava na maquina

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from src.load import salvarCsv, salvarSQlite, salvarMySql
 dadosBcb = requestApiBcb("20191")
 # salvarCsv(dadosBcb, "meiodePagamento.csv", ';', '.')
 
-# salvarSQlite(dadosBcb, "src/datasets/etlbcb.db","meios_pagamentos_tri")
+salvarSQlite(dadosBcb, "src/datasets/etlbcb.db","meios_pagamentos_tri")
 
-salvarMySql(dadosBcb, "root", "root", "localhost", "etlbcb", "meiospagamentostri")
+# salvarMySql(dadosBcb, "root", "root", "localhost", "etlbcb", "meiospagamentostri")
